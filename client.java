31eb3385af1f627bdad2fd70405dbf34e7603943
RitZZZZ <72753638+HAISHE-69@users.noreply.github.com>
2024-07-06 19:16:56 +0530

    Add files via upload

---
+++ client.java
@@ -0,0 +1,17 @@
+import java.net.*;
+import java.io.*;
+
+public class client{
+	public static void main(String[]args) throws IOException{
+	Socket s=new Socket("localhost",3000);
+PrintWriter pr=new PrintWriter(s.getOutputStream());
+pr.println("hello");
+pr.flush();
+
+InputStreamReader in=new InputStreamReader(s.getInputStream());
+BufferedReader bf=new BufferedReader(in);
+
+String str = bf.readLine();
+System.out.println("server :"+str);
+}
+}
